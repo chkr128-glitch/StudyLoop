3dd3bd57faae458969c582ecd7b1bf1f10da8869
akira <chkr128@gmail.com>
2026-08-19 22:26:18 +0900

    Update helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -21,3 +21,4 @@ export function escapeHTML(str) {
     return str ? str.replace(/[&<>'"]/g, tag => ({
         '&': '&amp;', '<': '&lt;', '>': '&gt;', "'": '&#39;', '"': '&quot;'
     }[tag])) : ''; 
+}
